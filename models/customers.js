f37d90c498b26ac934a0d241b8a0e74a1b9ba132
Akash Mule <akashmule341@gmail.com>
2024-03-20 18:39:27 +0530

    feat: 🎸 added seeders for data dumping

--- models/customers.js
+++ models/customers.js
@@ -9,6 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.hasMany(models.addresses, { as: 'FkCustomerId', constraints: false });
     }
   }
   Customers.init(
