c2450481136bec34ecd80a004467eb9fa96020de
Akash Mule <akashmule341@gmail.com>
2024-12-12 08:59:06 +0530

    fix: Resolved testing comments

--- routes/v1/store-routes.js
+++ routes/v1/store-routes.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { storesController } = require('../../controllers');
 // Route to get all todos
-router.get('/:id', storesController.getStoreById);
-router.get('/', storesController.getAllStores);
+
 // Route to create a new todo
 // router.post('/', storesController.createStore);
 // Route to get a todo by ID
-
+router.get('/', storesController.getAllStores);
 // Route to update a todo by ID
 // router.put('/:id', storesController.updateStore);
 // // Route to delete a todo by ID
 // router.delete('/:id', storesController.deleteStore);
+router.get('/:id', storesController.getStoreById);
+
 module.exports = router;
