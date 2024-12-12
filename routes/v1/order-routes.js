a86e354b05600cc01f494497e770ddf34970c43d
Akash Mule <akashmule341@gmail.com>
2024-12-12 08:57:18 +0530

    feat: provides delivery functionalty to users

--- routes/v1/order-routes.js
+++ routes/v1/order-routes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { ordersController, storesController } = require('../../controllers');
 
+router.get('/stores/:id', ordersController.getStoreOrdersByStoreId);
 router.get('/', ordersController.getAllOrders);
 router.get('/customer/:id', ordersController.getCustomerOrdersByCustomerId);
-router.get('/stores/:id', ordersController.getStoreOrdersByStoreId);
 
 module.exports = router;
